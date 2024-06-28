9b41eea97e203a048b0d5de3266f84bd4a9b5852
Seymour-ye <zhimo.ye@hotmail.com>
2024-06-28 19:25:09 -0400

    add language switch, add nav bar

---
+++ app/controllers/languages_controller.rb
@@ -0,0 +1,8 @@
+class LanguagesController < ApplicationController
+    def update
+      session[:locale] = params[:language]
+  
+      redirect_back fallback_location: root_path
+    end
+  end
+  
